9723a8e5ccbb4eab93332d7a6256172a41693d39
Ye-2077 <2390227785@qq.com>
2025-05-20 17:03:26 +0800

    test2

--- legged_gym/envs/pointfoot/flat/PF/pointfoot_flat_config.py
+++ legged_gym/envs/pointfoot/flat/PF/pointfoot_flat_config.py
@@ -43,4 +43,4 @@ class PointFootFlatCfgPPO(PointFootRoughCfgPPO):
 
     class runner(PointFootRoughCfgPPO.runner):
         experiment_name = 'pointfoot_flat'
-        max_iterations = 12000
+        max_iterations = 5000
