6f338ad41839bc57d6122b4a1ef4ca218bedb1d5
Ye-2077 <2390227785@qq.com>
2025-05-23 14:56:34 +0800

    test1-1

--- legged_gym/envs/pointfoot/flat/PF/pointfoot_flat_config.py
+++ legged_gym/envs/pointfoot/flat/PF/pointfoot_flat_config.py
@@ -16,10 +16,10 @@ class PointFootFlatCfg(PointFootRoughCfg):
         max_contact_force = 350.
 
         class scales(PointFootRoughCfg.rewards.scales):
-            orientation = -5.0
-            torques = -0.000025
+            orientation = 0 #-5.0
+            torques = 0 #-0.000025
             feet_air_time = 5.
-            unbalance_feet_air_time = 1.0
+            unbalance_feet_air_time = 0 #1.0
             no_fly = 1.
             # feet_contact_forces = -0.01
 
